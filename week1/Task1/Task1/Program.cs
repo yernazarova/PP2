e5fc5aeff04a9fb3648c236b4abbc1f6bd1f46f0
yernazarova <yernazarovam1@gmail.com>
2019-02-03 22:34:12 +0600

    lab1 added

--- week1/Task1/Task1/Program.cs
+++ week1/Task1/Task1/Program.cs
@@ -1,41 +1,37 @@
 ﻿using System;
 
-namespace Task1
+public class Program
 {
-    class Program
+
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string b;
+        b = Console.ReadLine();
+        int n = int.Parse(b);
+
+        string[] arr = Console.ReadLine().Split();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++)
         {
-            int n = int.Parse(Console.ReadLine());
-            string s = Console.ReadLine();
-            string[] arr = s.Split();
-            int cnt = 0;
-            for (int i = 0; i < n; i++)
-            {
-                int c = int.Parse(arr[i]);
-                if (Prime(c) == true)
+            int c = int.Parse(arr[i]);
+            int j;
+            for (j = 2; j < c; j++)
+                if ((c % j == 0))
                 {
-                    cnt++;
+                    break;
                 }
-            }
-            Console.WriteLine(cnt);
 
-            for (int i = 0; i < n; i++)
+
+            if (j == c)
             {
-                int c = int.Parse(arr[i]);
-                int j;
-                for (j = 2; j < c; j++)
-                    if ((c % j == 0))
-                    {
-                        break;
-                    }
-
-
-                if (j == c)
-                {
-                    Console.Write(c + " ");
-                }
+                Console.Write(c + " ");
             }
+
+
+            }
+
         }
+       
+
     }
-}
---
+++ week1/task3/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace b
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            string n; //объявляем переменную   
+            n = Console.ReadLine(); //считываем строку
+            int b = int.Parse(n);//переобразовываем  массив в integer
+
+            string[] arr = Console.ReadLine().Split(); // создаем массив, считываем и разделяем его по пробелу
+
+
+            for (int i = 0; i < b; i++) //создаем цикл для массива с длинною б
+            {
+
+                Console.Write(arr[i] + " " + arr[i] + " ");//выводим два раза повторяющийся элемент, который разделен между собой пробелом
+            }
+        }
+    }
+}
