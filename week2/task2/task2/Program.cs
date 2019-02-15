99364f1b3a47fb3fca5a1d03fb07fe563c4baee4
yernazarova <yernazarovam1@gmail.com>
2019-02-15 11:30:13 +0600

    lab2 added

---
+++ week2/task2/task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace task2
+{
+    class MainClass
+    {
+        static bool IsPrime(int n) //вызываем функция булеан 
+        {
+            if (n == 1) 
+                return false;
+            for (int m = 2; m < n; m++) //считываем массив,1 не прайм, начинаем с2
+            {
+                if (n % m == 0) //если число делиться еще на число кроме себя 
+                    return false;
+            }
+            return true; //если условия не выполнены то число прайм
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo(@"/Users/meruyertyernazarova/desktop/pp2/week2/task2"); //получаем путь до папки
+            StreamReader sr = new StreamReader("input.txt"); //путь с папки до файла инпут
+            StreamWriter sw = new StreamWriter("output.txt");//путь с папки до оутпут, куда будем вставлять прайм намберс
+            string s = sr.ReadLine(); //считываем строку файла инпут созданную как переменная 
+            string[] b= s.Split(); //создаем новый массив,разделяем его по пробелам
+
+            for (int i = 0; i < b.Length; i++) //создаем цикл,считываем нашу строку в массиве
+            {
+                int f = int.Parse(b[i]); //преобразовываем ее в инт
+                if (IsPrime(f) == true) // если по условию оно прайм
+                {
+                    sw.Write(f + " "); //то записываем прайм числа в оутпут
+                }
+
+
+            }
+            sr.Close(); //закрываем файлы
+            sw.Close();
+        }
+    }
+}
---
+++ week2/task4/task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class MainClass
+    {
+        public static void Copy()
+        {
+            //задаем путь для первого файла
+            string path = "/Users/merruyeryernazarova/desktop/fortask4";
+
+            //задаем путь для второго файла(его копии)
+            string path1 = "/Users/meruyertyernazarova/desktop/fortask4/path";
+
+            //объявляем новые файлы и указываем путь для них
+            string text = Path.Combine(path, "text.txt");
+            string copy = Path.Combine(path1, "copy.txt");
+
+            //создаем первый файл
+            File.Create(text);
+
+            //создаем ее копию
+            File.Copy(text, copy, true);
+
+            //удаляем оригинал
+            File.Delete(text);
+        }
+
+        public static void Main(string[] args)
+        {
+            //вызываем функцию
+            Copy();
+        }
+    }
+}
