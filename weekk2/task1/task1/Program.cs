86d96a7d90659261e6fc815fe65beec8c9b41384
yernazarova <yernazarovam1@gmail.com>
2019-02-15 11:42:30 +0600

    lab2 added

---
+++ weekk2/task1/task1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace Task1
+{
+    class MainClass
+    {
+        //создаем функцию, которая возвращает true, false после выполнения условия 
+        public static bool P(string s)
+        {
+            //объявляем переменные: начало слово и конец
+            int i = 0;
+            int j = s.Length - 1;
+
+            //условие: пока i меньше j(пока отсчет дойдет до середины)
+            while (i < j)
+            {
+                //если значение i-того и j-того элемента не равны, возвращать false
+                if (s[i] != s[j])
+                    return false;
+                //увеличиваем значение i и уменьшаем j, чтобы проверить полностью слово
+                i++;
+                j--;
+            }
+
+            //если указанное выше условие не выполняется, возвращаем true
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            DirectoryInfo d = new DirectoryInfo("/Users/meruyertyernazarova/desktop");
+            StreamReader sr = new StreamReader("a.txt");
+            String t = sr.ReadLine(); //создаем переменную для строки
+
+            Console.WriteLine(t); //считываем
+
+            bool a = P( t); 
+
+            //если условие выполнено, выводим "YES"
+            if (a == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            //в противном случае выводим "NO"
+            else
+            {
+                Console.WriteLine("NO");
+
+            }
+        }
+    }
+}
---
+++ weekk2/task3/task3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Task3
+{
+    class MainClass
+    {
+
+        //создаем функцию, которая будет выводить пробелы
+        static void PrintSpaces(int level)
+        {
+            //создаем цикл для вывода
+            for (int i = 0; i < level; i++)
+            {
+                //выводим пробелы
+                Console.Write("   ");
+            }
+        }
+
+        //создаем функцию, которая будет выводить название файлов из заданного пути
+        static void Direction(DirectoryInfo directory, int level)
+        {
+            //создаем Fileinfo, чтобы получить файлы 
+            FileInfo[] files = directory.GetFiles();
+
+            //создаем цикл для файлов
+            foreach (FileInfo file in files)
+            {
+                //вызываем функцию для вывода пробелов с уровнем "level" 
+                PrintSpaces(level);
+
+                //выводим в кансоль название файла с заданном пути
+                Console.WriteLine(file.Name);
+            }
+
+            //создаем DirectoryInfo, чтобы получить путь ко всем файлам 
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+            //создаем цикл для путей
+            foreach (DirectoryInfo d in directories)
+            {
+                //вызываем функцию для вывода пробелов с уровнем "level"
+                PrintSpaces(level);
+
+                //выводим в кансоль название папки с заданного пути
+                Console.WriteLine(d.Name);
+
+                //рекурсивно вызываем вторую функцию, увеличивая значение на 1
+                Direction(d, level + 1);
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            //создаем DirectoryInfo и задаем свой путь 
+            DirectoryInfo path = new DirectoryInfo("/Users/meruyertyernazarova/desktop/pp2/week2");
+
+            //вызываем фунцию для вывода файлов и передаем наше значение: путь и количество пробелов
+            Direction(path, 0);
+        }
+    }
+}
